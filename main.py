994ff29cf6c836b96cdf39246c099cea36afa5d1
kosne1 <akolia2017@list.ru>
2024-11-19 16:32:48 +0300

    feat: add send 'start' message

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("abob")
+from telebot import TeleBot
+from telebot.types import Message
+
+bot = TeleBot(token='6454617316:AAHfZvI7FCOLV8V4wQ9EqBLmlzn_6DXXWhM', parse_mode='html')
+
+
+@bot.message_handler(commands=['start'])
+def start(message: Message):
+    bot.send_message(chat_id=message.chat.id, text='Привет, как у тебя дела?')
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
