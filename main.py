3bc15ee626af00671cd94f57e2ad54cd458b56f1
kosne1 <akolia2017@list.ru>
2025-03-02 19:04:32 +0300

    feat: add conditions on "/start"

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from docx import Document
 from telebot import TeleBot
-from telebot.apihelper import send_message
 from telebot.types import Message
 
 from models import Users
@@ -13,16 +11,25 @@ def start(message: Message):
     user, created = Users.get_or_create(
         tg_id=message.from_user.id, username=message.from_user.username
     )
-    if user.first_name:
-        bot.send_message(chat_id=message.chat.id, text="А фамилия какая у тебя?🥺")
-        bot.register_next_step_handler(message, ur_adress)
-    else:
+    if user.first_name is None:
         bot.send_message(chat_id=message.chat.id, text="Приветь)😜😙😙 Как тебя зовут?)🤔🥺🥺")
         bot.register_next_step_handler(message, what_is_ur_surname)
+    elif user.last_name is None:
+        bot.send_message(chat_id=message.chat.id, text="А фамилия какая у тебя?🥺")
+        bot.register_next_step_handler(message, ur_age)
+    elif user.age is None:
+        bot.send_message(chat_id=message.chat.id, text="А сколько тебе лет..?")
+        bot.register_next_step_handler(message, povestka)
+    else:
+        if user.age >= 18:
+            bot.send_message(chat_id=message.chat.id, text="Хаха, мы тебя уже вычислили!")
+            bot.send_photo(chat_id=message.chat.id,
+                           photo="https://yandex.ru/images/search?from=tabbar&img_url=https%3A%2F%2Favatars.dzeninfra.ru%2Fget-zen_doc%2F271828%2Fpub_65618a0988410d6f4aa2873e_65618b73654b6f493a9b1b92%2Fscale_1200&lr=2&pos=2&rpt=simage&text=%D0%BF%D0%BE%D0%B2%D0%B5%D1%81%D1%82%D0%BA%D0%B0")
+        else:
+            bot.send_message(chat_id=message.chat.id, text="База, разходимся, он не совершеннолетний...")
 
 
 def what_is_ur_surname(message: Message):
-    print(type(int("17")))
     Users.update(first_name=message.text).where(Users.tg_id == message.from_user.id).execute()
     bot.send_message(chat_id=message.chat.id, text="Ой какое имя красивое))😍😇 А фамилия какая у тебя?🥺")
     bot.register_next_step_handler(message, ur_adress)
@@ -40,16 +47,17 @@ def ur_age(message: Message):
 
 
 def povestka(message: Message):
-     if message.text.isdigit():
-         Users.update(age=int(message.text)).where(Users.tg_id == message.from_user.id).execute()
-         if message.text >= f"{18}":
-             bot.send_message(chat_id=message.chat.id, text="Оооооо братик ну ты попал~ Лови повестку!")
-             bot.send_photo(chat_id=message.chat.id,
-                            photo="https://yandex.ru/images/search?from=tabbar&img_url=https%3A%2F%2Favatars.dzeninfra.ru%2Fget-zen_doc%2F271828%2Fpub_65618a0988410d6f4aa2873e_65618b73654b6f493a9b1b92%2Fscale_1200&lr=2&pos=2&rpt=simage&text=%D0%BF%D0%BE%D0%B2%D0%B5%D1%81%D1%82%D0%BA%D0%B0")
-         else:
-             bot.send_message(chat_id=message.chat.id, text="База, разходимся, он не совершеннолетний...")
-     else:
-         bot.send_message(chat_id=message.chat.id, text="Литературно выражайся, хлопчик.")
+    if message.text.isdigit():
+        Users.update(age=int(message.text)).where(Users.tg_id == message.from_user.id).execute()
+        if int(message.text) >= 18:
+            bot.send_message(chat_id=message.chat.id, text="Оооооо братик ну ты попал~ Лови повестку!")
+            bot.send_photo(chat_id=message.chat.id,
+                           photo="https://yandex.ru/images/search?from=tabbar&img_url=https%3A%2F%2Favatars.dzeninfra.ru%2Fget-zen_doc%2F271828%2Fpub_65618a0988410d6f4aa2873e_65618b73654b6f493a9b1b92%2Fscale_1200&lr=2&pos=2&rpt=simage&text=%D0%BF%D0%BE%D0%B2%D0%B5%D1%81%D1%82%D0%BA%D0%B0")
+        else:
+            bot.send_message(chat_id=message.chat.id, text="База, разходимся, он не совершеннолетний...")
+    else:
+        bot.send_message(chat_id=message.chat.id, text="Литературно выражайся, хлопчик.")
 
-if __name__ == '__main__':
+
+if __name__ == 'main':
     bot.polling(none_stop=True)
